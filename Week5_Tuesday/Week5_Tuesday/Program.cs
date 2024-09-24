d926cc5e65fab13f471f8c3572886cc54f62d1ce
Tommy Seternus <thomassseternus@gmail.com>
2024-09-24 09:01:01 -0400

    Add files via upload

---
+++ Week5_Tuesday/Week5_Tuesday/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Week5_Tuesday
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            int myFavoriteNumber = 3;
+            int theBestNumber = 17;
+
+            int sum;
+
+            sum = myFavoriteNumber + theBestNumber;
+
+            Console.WriteLine(sum);
+        }
+    }
+}
